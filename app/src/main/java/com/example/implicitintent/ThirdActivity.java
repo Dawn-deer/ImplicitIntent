a13519bf9777d4ad7a22a3fcce96a0eb1abca73e
Dawn-deer <2236495861@qq.com>
2020-11-03 17:30:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/implicitintent/ThirdActivity.java
@@ -0,0 +1,16 @@
+package com.example.implicitintent;
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
